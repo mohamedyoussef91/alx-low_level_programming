cd09ebaf2d1b3592d0ccf90de1824b2ac19afba5
mohamedyoussef91 <mohamedrabie911@gmail.com>
2023-08-10 15:43:07 -0700

    last

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descraption: A C program that prints with funtion
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
