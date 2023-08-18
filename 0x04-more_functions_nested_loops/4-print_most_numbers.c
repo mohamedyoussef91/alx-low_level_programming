1fd4601a9d716a061f504707534f34c5b8a0a038
mohamedyoussef91 <mohamedrabie911@gmail.com>
2023-08-18 15:50:58 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 - 9 apart from 2 and 4 and you can only use _putchar twice
+ * print_most_numbers - print 0 - 9
+ * apart from 2 and 4 and you can only use _putchar twice
  *
  * Return: Alwaays 0 (success)
 */
