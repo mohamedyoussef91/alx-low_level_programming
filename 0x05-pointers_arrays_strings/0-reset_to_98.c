44a8731e4cd16e8b56d082a8a53ddd8f6626c223
mohamedyoussef91 <mohamedrabie911@gmail.com>
2023-08-21 12:23:25 -0700

    1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to am int paraometer
+ *		and update the value is point to 98
+ *
+ * @n: int parameter
+ *
+ * return: nothing
+*/
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
