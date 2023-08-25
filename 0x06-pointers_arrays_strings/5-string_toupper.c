eb5f845828d952dd0067163946dbaeb6942263a5
mohamedyoussef91 <mohamedrabie911@gmail.com>
2023-08-25 14:07:07 -0700

    1

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *string_toupper - changes all lowercase letters of a string to upper
+ * string_toupper - changes all lowercase letters of a string to upper
  *
  * @str: string to return
  *
