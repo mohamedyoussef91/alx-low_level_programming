6d8cbdb3e36d3ebb73d54250d345f2c42d50d0d3
mohamedyoussef91 <mohamedrabie911@gmail.com>
2023-08-28 12:09:38 -0700

    test

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "home.h"
+
+/**
+ * *_strpbrk -  function locates the first occurrence in the string
+ * @s: string
+ * @accept: string to match
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no such byte is found
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int itr, jtr;
+	char *p;
+
+	itr = 0;
+	while (s[itr] != '\0')
+	{
+		jtr = 0;
+		while (accept[jtr] != '\0')
+		{
+			if (accept[jtr] == s[itr])
+			{
+				p = &s[itr];
+				return (p);
+			}
+			jtr++;
+		}
+		itr++;
+	}
+
+	return (0);
+}
