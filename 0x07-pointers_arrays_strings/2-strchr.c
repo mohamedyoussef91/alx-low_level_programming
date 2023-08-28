8e0d597f76320b3bfac97c08792927b60c2a036e
mohamedyoussef91 <mohamedrabie911@gmail.com>
2023-08-28 11:50:28 -0700

    test

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,14 +4,14 @@
  * *_strchr - fills memory with a constant byte
  * @s: pointer to put the constant
  * @c: constant
- * Return: pionter to s
+ * Return: pionter to 0
 */
 
 char *_strchr(char *s, char c)
 {
 	int itr;
 
-	for (itr = 0; s[itr] >= '\0' ; itr++)
+	for (itr = 0; s[itr] >= '\0'; itr++)
 	{
 			if (s[itr] == c)
 			{
