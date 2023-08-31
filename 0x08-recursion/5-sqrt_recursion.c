094910fa9602fc62532066274742ef88318136bc
mohamedyoussef91 <mohamedrabie911@gmail.com>
2023-08-31 15:23:23 -0700

    test

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,10 +22,12 @@ int _sqrt_recursion(int n)
 
 int square(int n, int val)
 {
-	if (val * val ==n)
+
+	if (val * val == n)
 		return (val);
 	else if (val * val < n)
 		return (square(n, val + 1));
 	else
 		return (-1);
+
 }
